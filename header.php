b6fa656ab8659f5e51c33b1d6e6129365a6553d7
Rodolfo Marques <rodolfomarques87@gmail.com>
2019-09-23 21:42:43 -0300

    iniciando Rerpositório

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema de Gestão de TCCs</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <section id="header">
+        <h1 id="main_title">Sistema de Gerenciamento de Trabalhos de Conlusão de Curso</h1>
+    </section>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include "header.php";?>
+
+    <div id="main_container">
+        <div class="card">
+            <h2 class="sub_title">Seja bem vindo ao nosso sistema. O que você gostaria de fazer</h2>
+            <form action="" method="post">
+                <ul id="first_menu">
+                    <li class="menu_item"><a href="#" class="menu_link">Cadastrar Usuário</a></li>
+                    <li class="menu_item"><a href="#" class="menu_link">Cadastrar TCC </a></li>
+                    <li class="menu_item"><a href="#" class="menu_link">Pesquisar TCC</a></li>
+                </ul>
+
+                <button type='button' name=></button>
+            </form>
+        </div>
+    </div>
+
+
+
+<?php 
+
+
+
+include "footer.php";?>
